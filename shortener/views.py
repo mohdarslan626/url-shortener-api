d1a5994797d6689371f85bd30e1c0bd62ed2c74f
MOHD ARSLAN <64858711+QUANTUM00007@users.noreply.github.com>
2026-08-08 14:44:12 +0530

    refactor: organize API views into separate modules

--- shortener/views.py
+++ shortener/views.py
@@ -1,21 +1,3 @@
-# Django
-from django.db.models import Q
-from django.http import HttpResponseGone
-from django.shortcuts import get_object_or_404, redirect
-from django.utils import timezone
-
-# Django REST Framework
-from rest_framework import generics, status
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from rest_framework.views import APIView
-
-# Local imports
-from .models import ShortURL
-from .pagination import ShortURLPagination
-from .permissions import IsOwner
-from .serializers import ShortURLSerializer
-
 # API modules
 from .api.dashboard import DashboardView
 from .api.redirect import redirect_url
